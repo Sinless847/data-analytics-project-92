fdd5177a6b13fb8a82fd0be23f93d8f4ebadf482
Sinless847 <19syrax97@gmail.com>
2025-09-24 20:54:42 +0400

    queries.sql

--- queries.sql
+++ queries.sql
@@ -90,12 +90,12 @@ ORDER BY
 WITH age_groups AS (
     SELECT
         CASE
-            WHEN c.age BETWEEN 16 AND 25 THEN '16-25'
-            WHEN c.age BETWEEN 26 AND 40 THEN '26-40'
-            WHEN c.age > 40 THEN '40+'
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            WHEN age > 40 THEN '40+'
         END AS age_category
     FROM
-        customers AS c
+        customers
 )
 
 SELECT
