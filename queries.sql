ce81e6ada0c72b5b4c2d0d7b4d39825a4019144f
Sinless847 <19syrax97@gmail.com>
2025-09-25 21:32:50 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,7 @@
--- Напишите запрос, который считает общее количество покупателей из таблицы customers.
+-- top_10_profitable_products.csv
 SELECT COUNT(*) AS customers_count
 FROM
     customers;
-
 --top_10_total_income.csv
 SELECT
     e.first_name || ' ' || e.last_name AS seller,
@@ -16,32 +15,6 @@ INNER JOIN products AS p
 GROUP BY e.employee_id, e.first_name, e.last_name
 ORDER BY income DESC
 LIMIT 10;
-
---lowest_average_income.csv
-WITH seller_stats AS (
-    SELECT
-        e.first_name || ' ' || e.last_name AS seller,
-        SUM(s.quantity * p.price) AS total_income,
-        COUNT(s.sales_id) AS operations,
-        SUM(s.quantity * p.price) / COUNT(s.sales_id) AS avg_income_per_sale
-    FROM sales AS s
-    INNER JOIN employees AS e ON s.sales_person_id = e.employee_id
-    INNER JOIN products AS p ON s.product_id = p.product_id
-    GROUP BY e.employee_id, e.first_name, e.last_name
-),
-
-overall_avg AS (
-    SELECT SUM(total_income)::numeric / SUM(operations) AS overall_avg_income
-    FROM seller_stats
-)
-
-SELECT
-    seller,
-    FLOOR(avg_income_per_sale) AS average_income
-FROM seller_stats, overall_avg
-WHERE avg_income_per_sale < overall_avg_income
-ORDER BY average_income ASC;
-
 --day_of_the_week_income.csv
 SELECT
     e.first_name || ' ' || e.last_name AS seller,
@@ -59,7 +32,6 @@ GROUP BY
     TO_CHAR(s.sale_date, 'FMday'),
     TO_CHAR(s.sale_date, 'ID')
 ORDER BY TO_CHAR(s.sale_date, 'ID')::int, seller;
-
 --age_groups.csv
 WITH age_groups AS (
     SELECT
@@ -76,7 +48,6 @@ SELECT
     COUNT(*) AS age_count
 FROM age_groups
 GROUP BY age_category;
-
 --customers_by_month.csv
 SELECT
     TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
@@ -86,7 +57,6 @@ FROM sales AS s
 INNER JOIN products AS p ON s.product_id = p.product_id
 GROUP BY TO_CHAR(s.sale_date, 'YYYY-MM')
 ORDER BY selling_month;
-
 --special_offer.csv
 WITH first_sales AS (
     SELECT
@@ -111,3 +81,21 @@ INNER JOIN customers AS c ON fs.customer_id = c.customer_id
 INNER JOIN employees AS e ON fs.sales_person_id = e.employee_id
 WHERE fs.rn = 1
 ORDER BY c.customer_id;
+--lowest_average_income.csv
+SELECT
+    e.first_name || ' ' || e.last_name AS seller,
+    ROUND(AVG(p.price * s.quantity)) AS average_income
+FROM sales AS s
+INNER JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+INNER JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY e.employee_id, e.first_name, e.last_name
+HAVING
+    AVG(p.price * s.quantity) < (
+        SELECT AVG(p2.price * s2.quantity)
+        FROM sales AS s2
+        INNER JOIN products AS p2
+            ON s2.product_id = p2.product_id
+    )
+ORDER BY average_income ASC;
