9a19bc1df2965a07c64f479b536474feabd8a1e4
Sinless847 <19syrax97@gmail.com>
2025-09-28 18:32:07 +0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -35,7 +35,7 @@ SELECT
     CASE
         WHEN age BETWEEN 16 AND 25 THEN '16-25'
         WHEN age BETWEEN 26 AND 40 THEN '26-40'
-        WHEN age > 40 THEN '40+'
+        ELSE '40+'
     END AS age_category,
     COUNT(*) AS age_count
 FROM customers
@@ -43,7 +43,7 @@ GROUP BY
     CASE
         WHEN age BETWEEN 16 AND 25 THEN '16-25'
         WHEN age BETWEEN 26 AND 40 THEN '26-40'
-        WHEN age > 40 THEN '40+'
+        ELSE '40+'
     END;
 --customers_by_month.csv
 SELECT
